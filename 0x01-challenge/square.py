c9977c73290bab46042ea66788acd9edd83020b0
ilias <iliaschokairi@gmail.com>
2024-04-13 19:23:21 +0000

    square width and height are equals

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,18 +7,17 @@ class square():
     width = 0
     height = 0
 
-    def __init__(self, *args, **kwargs):
-        """instantiation"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """the perimetre of the square"""
-        return (self.width * 2) + (self.height * 2)
+        """Calculate the perimeter of the square."""
+        return (4 * self.width)
 
     def __str__(self):
         """representaion of the square"""
@@ -27,7 +26,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=12)
+    s = square(width=12, height=8)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
