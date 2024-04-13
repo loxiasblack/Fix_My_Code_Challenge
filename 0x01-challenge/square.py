e402e76dcb9697de82fff93fe1176305438b07e1
ilias <iliaschokairi@gmail.com>
2024-04-13 11:02:50 +0000

    square/rectangle with pycodestylii

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""my square rectangle"""
+
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         if (kwargs):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-    
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
@@ -21,6 +21,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
